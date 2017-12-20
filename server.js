6444a58d55d7c446125c125fc2e575f7e478d8f5
wangze <wangze@embway.com>
2017-12-20 17:25:09 +0800

    master key

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const AV = require('leancloud-storage')
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID,
   appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
 })
 
 const app = require('./app')
