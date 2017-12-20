e1dcd8670d2eea42195078914977b9342fadde1f
wangze <wangze@embway.com>
2017-12-20 17:08:35 +0800

    port

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const app = 
+const Koa = require('koa')
+const Ctrl = require('koa-route');
+const koaBody = require('koa-body');
+const app = new Koa();
+const AV = require('leanengine')
+
+app.use(koaBody())
+app.use(AV.koa())
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const APP_ID = 'UWHu3d90DN5PzYbXknr5deJA-gzGzoHsz'
-const APP_KEY = 'myMByAL64VApDxsanVtwLhrW'
 const AV = require('leancloud-storage')
 
 AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
+  appId: process.env.LEANCLOUD_APP_ID || 'UWHu3d90DN5PzYbXknr5deJA-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'myMByAL64VApDxsanVtwLhrW',
 })
 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function (object) {
-  alert('LeanCloud Rocks!');
+const app = require('./app')
+
+const PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000)
+app.listen(PORT, err => {
+  console.log(`Node app is running on port: ${PORT}`)
+
+  // 注册全局未捕获异常处理器
+  process.on('uncaughtException', function (err) {
+    console.error('Caught exception:', err.stack);
+  });
+  process.on('unhandledRejection', function (reason, p) {
+    console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
+  });
 })
+
+// leancloud-storage 写入测试代码
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'Hello World!'
+// }).then(function (object) {
+//   console.log('LeanCloud Rocks!');
+// })
