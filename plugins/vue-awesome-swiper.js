f423f70bea9fe1471783f5edbb3cbac8fe768e9a
wangze <wangze@embway.com>
2017-12-28 17:35:35 +0800

    p3

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr'
-console.log('------------',process.browser)
-if (process.browser) {
-  Vue.use(VueAwesomeSwiper)
-}
+console.log(Vue)
+console.log(VueAwesomeSwiper)
+Vue.use(VueAwesomeSwiper)
